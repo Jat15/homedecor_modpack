59175d4d2ac3663844ebaa9f7171398bc23e57fb
Jat15 <jinan15@hotmail.com>
2017-08-24 17:36:21 +0200

    Building_blocks
    Reorganization mod
    Create function for moreblocks, stairs, node
    Add  all glass in cutting
    Alias for  compatibility

---
+++ building_blocks/node_stairs.lua
@@ -0,0 +1,225 @@
+local S = homedecor_i18n.gettext
+
+minetest.register_node("building_blocks:Fireplace", {
+	description = S("Fireplace"),
+	tiles = {
+		"building_blocks_cast_iron.png",
+		"building_blocks_cast_iron.png",
+		"building_blocks_cast_iron.png",
+		"building_blocks_cast_iron_fireplace.png"
+	},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = default.LIGHT_MAX,
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {cracky=2},
+})
+
+function building_blocks_stairs(nodename, def)
+	
+	local mod = string.match (nodename,"(.+):")
+	local name = string.match (nodename,":(.+)")
+	
+	minetest.register_node(nodename, def)
+
+	for i,groupname in ipairs(def.groups) do
+		if groupname == not("cracky") or not("choppy") or not("flammable") or not("crumbly") or not("snappy") then
+			table.remove(def[groups][groupname])
+		end
+    end
+	
+	if minetest.get_modpath("moreblocks") then
+		stairsplus:register_all(
+			mod,
+			name,
+			nodename,
+			{
+				description = def.description,
+				tiles = def.tiles,
+				groups = def.groups,
+				sounds = def.sounds,
+			}
+		)
+	else
+	
+		bb_stairs = {}
+		
+		-- Node will be called stairs:stair_<subname>
+		function bb_stairs.register_stair(subname, recipeitem, groups, images, description)
+			minetest.register_node("building_blocks:stair_" .. subname, {
+				description = description,
+				drawtype = "nodebox",
+				tiles = images,
+				paramtype = "light",
+				paramtype2 = "facedir",
+				is_ground_content = true,
+				groups = groups,
+				node_box = {
+					type = "fixed",
+					fixed = {
+						{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+						{-0.5, 0, 0, 0.5, 0.5, 0.5},
+					},
+				},
+			})
+
+			minetest.register_craft({
+				output = 'building_blocks:stair_' .. subname .. ' 4',
+				recipe = {
+					{recipeitem, "", ""},
+					{recipeitem, recipeitem, ""},
+					{recipeitem, recipeitem, recipeitem},
+				},
+			})
+
+			-- Flipped recipe for the silly minecrafters
+			minetest.register_craft({
+				output = 'building_blocks:stair_' .. subname .. ' 4',
+				recipe = {
+					{"", "", recipeitem},
+					{"", recipeitem, recipeitem},
+					{recipeitem, recipeitem, recipeitem},
+				},
+			})
+		end
+
+		-- Node will be called stairs:slab_<subname>
+		function bb_stairs.register_slab(subname, recipeitem, groups, images, description)
+			minetest.register_node("building_blocks:slab_" .. subname, {
+				description = description,
+				drawtype = "nodebox",
+				tiles = images,
+				paramtype = "light",
+				is_ground_content = true,
+				groups = groups,
+				node_box = {
+					type = "fixed",
+					fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+				},
+				selection_box = {
+					type = "fixed",
+					fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+				},
+			})
+
+			minetest.register_craft({
+				output = 'building_blocks:slab_' .. subname .. ' 3',
+				recipe = {
+					{recipeitem, recipeitem, recipeitem},
+				},
+			})
+		end
+
+		-- Nodes will be called stairs:{stair,slab}_<subname>
+		function bb_stairs.register_stair_and_slab(subname, recipeitem, groups, images, desc_stair, desc_slab)
+			bb_stairs.register_stair(subname, recipeitem, groups, images, desc_stair)
+			bb_stairs.register_slab(subname, recipeitem, groups, images, desc_slab)
+		end
+	
+		bb_stairs.register_stair_and_slab(name,nodename,
+			def.groups,
+			def.tiles,
+			S("%s Stair"):format(S(def.description)),
+			S("%s Slab"):format(S(def.description))
+		)
+		
+	end
+end
+	
+building_blocks_stairs("building_blocks:grate",{
+		drawtype = "glasslike",
+		description = S("Grate"),
+		tiles = {"building_blocks_grate.png"},
+		inventory_image = minetest.inventorycube("building_blocks_grate.png"),
+		paramtype = "light",
+		sunlight_propagates = true,
+		is_ground_content = true,
+		groups = {cracky=1},
+		sounds = default.node_sound_metal_defaults(),
+})
+
+building_blocks_stairs("building_blocks:Marble", {
+	description = S("Marble"),
+	tiles = {"building_blocks_marble.png"},
+	is_ground_content = true,
+	groups = {cracky=3, marble = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+building_blocks_stairs("building_blocks:hardwood", {
+	tiles = {"building_blocks_hardwood.png"},
+	is_ground_content = true,
+	description = S("Hardwood"),
+	groups = {choppy=1,flammable=1},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+building_blocks_stairs("building_blocks:fakegrass", {
+	tiles = {"default_grass.png", "default_dirt.png", "default_dirt.png^default_grass_side.png"},
+	description = S("Fake Grass"),
+	is_ground_content = true,
+	groups = {crumbly=3},
+	sounds = default.node_sound_dirt_defaults({
+		footstep = {name="default_grass_footstep", gain=0.4},
+	}),
+})
+
+building_blocks_stairs("building_blocks:Tar", {
+	description = S("Tar"),
+	tiles = {"building_blocks_tar.png"},
+	is_ground_content = true,
+	groups = {crumbly=1, tar_block = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+building_blocks_stairs("building_blocks:grate", {
+	drawtype = "glasslike",
+	description = S("Grate"),
+	tiles = {"building_blocks_grate.png"},
+	inventory_image = minetest.inventorycube("building_blocks_grate.png"),
+	paramtype = "light",
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {cracky=1},
+	sounds = default.node_sound_metal_defaults(),
+})
+
+building_blocks_stairs("building_blocks:Adobe", {
+	tiles = {"building_blocks_Adobe.png"},
+	description = S("Adobe"),
+	is_ground_content = true,
+	groups = {crumbly=3},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+building_blocks_stairs("building_blocks:Roofing", {
+	tiles = {"building_blocks_Roofing.png"},
+	is_ground_content = true,
+	description = S("Roof block"),
+	groups = {snappy=3},
+})
+
+building_blocks_stairs("building_blocks:smoothglass", {
+	drawtype = "glasslike",
+	description = S("Streak Free Glass"),
+	tiles = {"building_blocks_sglass.png"},
+	inventory_image = minetest.inventorycube("building_blocks_sglass.png"),
+	paramtype = "light",
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_glass_defaults(),
+})
+
+building_blocks_stairs("building_blocks:woodglass", {
+	drawtype = "glasslike",
+	description = S("Wood Framed Glass"),
+	tiles = {"building_blocks_wglass.png"},
+	inventory_image = minetest.inventorycube("building_blocks_wglass.png"),
+	paramtype = "light",
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_glass_defaults(),
+})
---
+++ building_blocks/other.lua
@@ -0,0 +1,127 @@
+local S = homedecor_i18n.gettext
+
+-- Node
+
+minetest.register_node("building_blocks:terrycloth_towel", {
+	drawtype = "raillike",
+	description = S("Terrycloth towel"),
+	tiles = {"building_blocks_towel.png"},
+	inventory_image = "building_blocks_towel_inv.png",
+	paramtype = "light",
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+                -- but how to specify the dimensions for curved and sideways rails?
+                fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
+	},
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {crumbly=3},
+})
+
+minetest.register_node("building_blocks:Tarmac_spread", {
+	drawtype = "raillike",
+	description = S("Tarmac Spread"),
+	tiles = {"building_blocks_tar.png"},
+	inventory_image = "building_blocks_tar_spread_inv.png",
+	paramtype = "light",
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+                -- but how to specify the dimensions for curved and sideways rails?
+                fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
+	},
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_dirt_defaults(),
+})
+
+minetest.register_node("building_blocks:BWtile", {
+	drawtype = "nodebox",
+	description = S("Chess board tiling"),
+	tiles = {
+		"building_blocks_BWtile.png",
+		"building_blocks_BWtile.png^[transformR90",
+		"building_blocks_BWtile.png^[transformR90",
+		"building_blocks_BWtile.png^[transformR90",
+		"building_blocks_BWtile.png",
+		"building_blocks_BWtile.png"
+	},
+	inventory_image = "building_blocks_bwtile_inv.png",
+	paramtype = "light",
+	walkable = false,
+	node_box = {
+		type = "fixed",
+		fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
+	},
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {crumbly=3},
+})
+
+minetest.register_node("building_blocks:brobble_spread", {
+	drawtype = "raillike",
+	-- Translators: "Brobble" is a portmanteau of "Brick" and "Cobble".
+	-- Translate however you see fit.
+	description = S("Brobble Spread"),
+	tiles = {"building_blocks_brobble.png"},
+	inventory_image = "building_blocks_brobble_spread_inv.png",
+	paramtype = "light",
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+                -- but how to specify the dimensions for curved and sideways rails?
+                fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
+	},
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {crumbly=3},
+})
+minetest.register_node("building_blocks:gravel_spread", {
+	drawtype = "raillike",
+	description = S("Gravel Spread"),
+	tiles = {"default_gravel.png"},
+	inventory_image = "building_blocks_gravel_spread_inv.png",
+	paramtype = "light",
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+                -- but how to specify the dimensions for curved and sideways rails?
+                fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
+	},
+	sunlight_propagates = true,
+	is_ground_content = true,
+	groups = {crumbly=2},
+	sounds = default.node_sound_dirt_defaults({
+		footstep = {name="default_gravel_footstep", gain=0.5},
+		dug = {name="default_gravel_footstep", gain=1.0},
+	}),
+})
+
+-- Craft Item
+
+minetest.register_craftitem("building_blocks:sticks", {
+	description = S("Small bundle of sticks"),
+	image = "building_blocks_sticks.png",
+	on_place_on_ground = minetest.craftitem_place_item,
+})
+
+minetest.register_craftitem("building_blocks:tar_base", {
+	description = S("Tar base"),
+	image = "building_blocks_tar_base.png",
+})
+
+-- Tool
+
+minetest.register_tool("building_blocks:knife", {
+	description = S("Tar Knife"),
+	inventory_image = "building_blocks_knife.png",
+	tool_capabilities = {
+		max_drop_level=0,
+		groupcaps={
+			choppy={times={[2]=7.50, [3]=2.80}, uses=100, maxlevel=1},
+			fleshy={times={[2]=5.50, [3]=2.80}, uses=100, maxlevel=1}
+		}
+	},
+})
